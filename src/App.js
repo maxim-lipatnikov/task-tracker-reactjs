6595eaf3f83d04924e6c33f932a88087829ea094
Максим Липатников <myulipatnikov@edu.hse.ru>
2021-04-19 22:07:53 +0300

    fixes_1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import styles from './styles.module.css';
 import React from 'react';
 
-// const Button = (props) => {
-//   const handleClick = () => {
-//     console.log('Task' + {tasks[0].id} + "completed status =" + {tasks[0].completed}")
-//   }
-//   return (
-//     <div>
-//       <button onClick = {handleClick}>{props.name}</button>
-//     </div>
-//   )
-// }
 
-
-let Task = (props) => {
-
+const Task = (props) => {
   const handleClick = () => {
     return (
       console.log('Task ' + props.id + ' completed status: ' + props.completed)
@@ -23,18 +11,15 @@ let Task = (props) => {
 
   return (
     <div className={styles.tasks}>
-      <div className={styles.name}>{props.name}</div>
-      <div className={styles.description}>{props.description}</div>
-      <div className={styles.completed}>Completed: {String(props.completed)}</div>
-    <button onClick={handleClick} className={styles.button}>DONE</button>
+      <div key={props.name} className={styles.name}>{props.name}</div>
+      <div key={props.description} className={styles.description}>{props.description}</div>
+      <div key={props.completed} className={styles.completed}>Completed: {String(props.completed)}</div>
+      <button onClick={handleClick} className={styles.button}>DONE</button>
     </div>
   )
 }
 
 class MyTodoList extends React.Component {
-
-
-
   state = {
     tasks: [
       { id: 1, name: 'Task Name 1', description: 'Task Description 1', completed: false },
@@ -46,14 +31,11 @@ class MyTodoList extends React.Component {
     ]
   }
 
-  render(
-    tasksItems = this.state.tasks.map(i => <Task id={i.id} name={i.name} description={i.description}
-      completed={i.completed} />)
-  ) {
+  render() {
     return (
       <div>
-        {tasksItems}
-        <Task onClick={this.handleClick} />
+        {this.state.tasks.map(i => <Task id={i.id} name={i.name} description={i.description}
+        completed={i.completed} />)}
       </div>
     )
   }
